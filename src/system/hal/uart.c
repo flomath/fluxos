ee5fc788cdcf2025ebd211b2b51bec3d83cba4f8
florian m <florian.mathis@students.fhv.at>
2015-03-13 11:22:35 +0100

    implemented uart programming model based on omap

--- src/system/hal/uart.c
+++ src/system/hal/uart.c
@@ -11,49 +11,51 @@
  * enable and configure rx/tx FIFO and DMA mode
  * and set protocol and baude rate
  */
-void init_uart(uint32_t port)
+void init_uart(uint32_t port, UARTConfiguration_t* config)
 {
-	// FIFO and DMA settings
+	// Reset the UART
+	uart_software_reset(port);
 
-	// Switch to config mode B
+	// FIFO and DMA settings
+	// 1. Switch to config mode B
 	uint8_t lcrValue = uart_write(port, UART_LCR_REG, UART_LCR_MODE_CONF_B);
 
-	// Enable register submode 1/2
+	// 2. Enable register submode 1/2
 	uint8_t efrValue = uart_set(port, UART_EFR_REG, UART_EFR_ENHANCED_EN);
 
-	// Switch to config mode A
+	// 3. Switch to config mode A
 	uart_write(port, UART_LCR_REG, UART_LCR_MODE_CONF_A);
 
-	// enable register submode 2/2
+	// 4. enable register submode 2/2
 	uint8_t mcrValue = uart_set(port, UART_MCR_REG, UART_MCR_TCR_TLR);
 
-	// Enable FIFO, load new FIFO Triggers 1/3 and set DMA-Mode 1/2
+	// 5. Enable FIFO, load new FIFO Triggers 1/3 and set DMA-Mode 1/2
 	uint8_t fifoMask = 0b01011001; // 16 char Trigger (RX/TX), DMA-Mode 1, FIFO-Enable
 	uart_set(port, UART_FCR_REG, fifoMask);
 
-	// Switch to config mode B
+	// 6. Switch to config mode B
 	uart_write(port, UART_LCR_REG, UART_LCR_MODE_CONF_B);
 
-	// load new FIFO triggers 2/3
+	// 7. load new FIFO triggers 2/3
 	uart_clear(port, UART_TLR_REG, 0xFF); // reset RX_FIFO_TRIG_DMA and TX_FIFO_TRIG_DMA to 0
 
-	// load new FIFO triggers 3/3 and set DMA-MODE 2/2
+	// 8. load new FIFO triggers 3/3 and set DMA-MODE 2/2
 	uart_set(port, UART_SCR_REG, UART_SCR_RX_TRIG_GRAN);
 	uart_set(port, UART_SCR_REG, UART_SCR_TX_TRIG_GRAN);
 	uart_clear(port, UART_SCR_REG, BV(2)); 	// DMA-Mode 1 (RX and TX)
 	uart_set(port, UART_SCR_REG, BV(1));	// DMA-Mode 1 (RX and TX)
 	uart_set(port, UART_SCR_REG, UART_SCR_DMA_MODE_CTL);
 
-	// restore enable register
+	// 9. restore enable register
 	uart_write(port, UART_EFR_REG, efrValue);
 
-	// Switch to config mode A
+	// 10. Switch to config mode A
 	uart_write(port, UART_LCR_REG, UART_LCR_MODE_CONF_A);
 
-	// Restore MCR Reg
+	// 11. Restore MCR Reg
 	uart_write(port, UART_MCR_REG, mcrValue);
 
-	// Restore LCR Reg
+	// 12. Restore LCR Reg
 	uart_write(port, UART_LCR_REG, lcrValue);
 
 
@@ -77,11 +79,17 @@ void init_uart(uint32_t port)
 	uart_write(port, UART_LCR_REG, UART_LCR_MODE_CONF_B);
 
 	// 7. Load new divisor value
+	// DLL_REG.CLOCK_LSB and DLH_REG.CLOCK_MSB
+	uint16_t divisor = CLK_UART / config->baude_rate;
+	uart_write(port, UART_DLL_REG, divisor); // LSB
+	uart_write(port, UART_DLH_REG, (divisor >> 8)); // MSB
 
 	// 8. Switch to register operational mode
 	uart_set(port, UART_LCR_REG, 0x00);
 
 	// 9. Load new interrupt configuration
+	// no other interrupts are enabled at this time
+	uart_set(port, UART_IER_REG, BV(0)); // RHR_IT enable (Data ready)
 
 	// 10. Switch to config mode B
 	uart_write(port, UART_LCR_REG, UART_LCR_MODE_CONF_B);
@@ -90,13 +98,61 @@ void init_uart(uint32_t port)
 	uart_write(port, UART_EFR_REG, efrValue2);
 
 	// 12. Load new protocol formatting
+	// DIV_EN to operational mode
+	// BREAK_EN to normal operating condition
+	uart_write(port, UART_LCR_REG, 0x00); // clear register
 
-	// 13. Load new UART mode
+	// set stop bit
+	if ( conf->stop_bit ) uart_set(port, UART_LCR_REG, BV(2));
+	
+	// set parity type
+	switch ( conf->parity_type ) 
+	{
+		case UART_PARITY_NONE:
+			break;
+
+		case UART_PARITY_0:
+			uart_set(port, UART_LCR_REG, BV(3)); // Parity enable
+			uart_set(port, UART_LCR_REG, BV(4)); // PARITY_TYPE_1 (Even)
+			uart_set(port, UART_LCR_REG, BV(5)); // PARITY_TYPE_2
+			break;
+
+		case UART_PARITY_1:
+			uart_set(port, UART_LCR_REG, BV(3)); // Parity enable
+			uart_set(port, UART_LCR_REG, BV(5)); // PARITY_TYPE_2
+			break;
+	}
+
+	// set char length
+	uart_write(port, UART_LCR_REG, uart_get_value(port, UART_LCR_REG) | conf->char_length);
+
+	// 13. Load new UART mode (set to UART 16x mode)
+	uart_clear(port, UART_MDR1_REG, 0x7);
+}
+
+/**
+ * Software reset
+ */
+void uart_software_reset(uint32_t port)
+{
+	uart_set(port, UART_SYSC_REG, BV(1));
+
+	// wait for reset
+	while(uart_get_value(port, UART_SYSS_REG) & BV(0) != 1) {}
 }
 
 mmio_t uart_get_register(uint32_t port, uint8_t offset)
 {
 	return (mmio_t)(port + offset);
+}
+
+/**
+ * Get value of given address
+ */
+uint8_t uart_get_value(uint32_t port, uint8_t offset) 
+{
+	mmio_t reg = uart_get_register(port, offset);
+	return *reg;
 }
 
 /**
--- src/system/hal/uart.h
+++ src/system/hal/uart.h
@@ -10,6 +10,8 @@
 
 #include "hal.h"
 
+#define CLK_UART		48000000
+
 #define UART1			0x4806A000 // [17-38]
 #define UART2			0x4806C000
 #define UART3			0x49020000
@@ -22,6 +24,10 @@
 #define UART_SCR_REG	0x040 // supplementary control register [17-116]
 #define UART_MDR1_REG	0x020 // Mode definition register [17-90]
 #define UART_IER_REG	0x004 // Interrupt enable register [17-46]
+#define UART_DLL_REG	0x000 // Divisor latches low [17-40
+#define UART_DLH_REG	0x004 // Divisor latches high [17-52]
+#define UART_SYSC_REG	0x054 // System configuration register [17-124]
+#define UART_SYSS_REG	0x058 // System status register [17-126]
 
 #define EN_UART1		BV(13)
 #define EN_UART2		BV(14)
@@ -29,6 +35,8 @@
 // UART_LCR_REG
 #define UART_LCR_MODE_CONF_A	0x80
 #define UART_LCR_MODE_CONF_B	0xBF
+#define UART_LCR_DIV_EN			BV(7) // also used for conf mode a/b
+#define UART_LCR_BREAK_EN		BV(6)
 
 // UART_EFR_REG
 #define UART_EFR_ENHANCED_EN	BV(4)
@@ -48,7 +56,37 @@ void uart_disable_uart1(void);
 uint8_t uart_write(uint32_t port, uint8_t offset, uint8_t mask);
 uint8_t uart_set(uint32_t port, uint8_t offset, uint8_t mask);
 uint8_t uart_clear(uint32_t port, uint8_t offset, uint8_t mask);
+uint8_t uart_get_value(uint32_t port, uint8_t offset);
+void uart_software_reset(uint32_t port);
 
 mmio_t uart_get_register(uint32_t port, uint8_t offset); // get register address
+
+/**
+ * LCR_REG Parity Options
+ * UART_PARITY_NONE: Parity disabled
+ * UART_PARITY_0: Paritybit 0 forced
+ * UART_PARITY_1: Paritybit 1 forced
+ */
+enum uart_parity_type {
+	UART_PARITY_NONE,
+	UART_PARITY_0,
+	UART_PARITY_1
+};
+/*
+ * LCR_REG character length
+ */
+enum uart_char_length {
+	UART_LENGTH_5 = 0x0,
+	UART_LENGTH_6 = 0x1,
+	UART_LENGTH_7 = 0x2,
+	UART_LENGTH_8 = 0x3
+};
+
+typedef struct {
+	uint8_t stop_bit;
+	uint32_t baude_rate;
+	uart_parity_type parity_type;
+	uart_char_length char_length;
+} UARTConfiguration_t;
 
 #endif /* SRC_SYSTEM_HAL_UART_H_ */
