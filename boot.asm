77d7a44ce1158aa2ea56e047d4c8b726a3fde94d
flomath <florian.mathis@students.fhv.at>
2015-03-18 12:12:20 +0100

    added boot.asm and started testing the UART settings

---
+++ boot.asm
@@ -0,0 +1,53 @@
+;*-------------------------------------------------------
+;* initial boot routine for fluxos
+;*-------------------------------------------------------
+	.armfunc _c_int00
+	.global	_c_int00
+
+;*-------------------------------------------------------
+;* Set main_func and args
+;*-------------------------------------------------------
+    .asg	_args_main,   	ARGS_MAIN_RTN
+    .asg	exit,         	EXIT_RTN
+    .asg    main_func_sp,	MAIN_FUNC_SP
+
+;*-------------------------------------------------------
+;* FunctionDef for c_int00
+;*-------------------------------------------------------
+_c_int00: .asmfunc
+
+	;*-------------------------------------------------------
+	;* Save current stack pointer for SDP analysis
+	;*-------------------------------------------------------
+	;	r0, c_mf_sp
+	;STR	sp, [r0]
+
+    ;*------------------------------------------------------
+    ;* Perform all the required initilizations:
+    ;*   - Process BINIT Table
+    ;*   - Perform C auto initialization
+    ;*   - Call global constructors
+    ;*------------------------------------------------------
+    BL      __TI_auto_init
+
+	;*-------------------------------------------------------
+	;* Call Application
+	;*-------------------------------------------------------
+    BL      ARGS_MAIN_RTN
+
+	;*-------------------------------------------------------
+	;* Call exit(1) if application didn't call it before
+	;*-------------------------------------------------------
+    MOV     R0, #1
+    BL      EXIT_RTN
+
+.end
+
+;******************************************************
+;* UNDEFINED REFERENCES                               *
+;******************************************************
+	.global ARGS_MAIN_RTN
+	.global MAIN_FUNC_SP
+	.global	EXIT_RTN
+	.global __TI_auto_init
+
--- main.c
+++ main.c
@@ -5,6 +5,19 @@
 #include "src/system/hal/uart.h"
 
 int main(void) {
+
+	UARTConfiguration_t conf = {
+			0x0,
+			115200,
+			UART_PARITY_0,
+			UART_LENGTH_8
+	};
+
+	init_uart(UART1, &conf);
+
+	while(1) {
+
+	}
 	
 	return 0;
 }
