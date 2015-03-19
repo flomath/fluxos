4d97114a847a44398cd9dd8d426067ed4c49317a
travis <travis@localhost>
2015-03-19 16:09:06 +0000

    Automatic doxygen build

--- doxygen/search/files_2.js
+++ doxygen/search/files_2.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['scm_2eh',['scm.h',['../scm_8h.html',1,'']]]
+  ['scm_2eh',['scm.h',['../scm_8h.html',1,'']]],
+  ['serialdriver_2eh',['SerialDriver.h',['../_serial_driver_8h.html',1,'']]]
 ];
