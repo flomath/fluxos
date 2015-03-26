723a83677f1203aabeb5784692b0585fa15ad243
Philip Heimböck <philipheimboeck@gmail.com>
2015-03-26 13:14:38 +0100

    Use 32Bitmask for Registers

--- src/system/hal/common/hal.c
+++ src/system/hal/common/hal.c
@@ -13,38 +13,31 @@ mmio_t hal_get_register(uint32_t port, uint8_t offset)
 }
 
 
-uint8_t hal_get_address_value(uint32_t port, uint8_t offset)
+uint32_t hal_get_address_value(uint32_t port, uint8_t offset)
 {
 	mmio_t reg = hal_get_register(port, offset);
 	return *reg;
 }
 
 
-uint8_t hal_bitmask_write(uint32_t port, uint8_t offset, uint8_t mask)
+uint32_t hal_bitmask_write(uint32_t port, uint8_t offset, uint32_t mask)
 {
 	mmio_t reg = hal_get_register(port, offset);
-	uint8_t value = *reg;
+	uint32_t value = *reg;
 	*reg = mask;
 
 	return value;
 }
 
-
-uint8_t hal_bitmask_set(uint32_t port, uint8_t offset, uint8_t mask)
+void hal_bitmask_set(uint32_t port, uint8_t offset, uint32_t mask)
 {
 	mmio_t reg = hal_get_register(port, offset);
-	uint8_t value = *reg;
 	*reg |= mask;
-
-	return value;
 }
 
 
-uint8_t hal_bitmask_clear(uint32_t port, uint8_t offset, uint8_t mask)
+void hal_bitmask_clear(uint32_t port, uint8_t offset, uint32_t mask)
 {
 	mmio_t reg = hal_get_register(port, offset);
-	uint8_t value = *reg;
 	*reg &= ~mask;
-
-	return value;
 }
