476be9e1608a2cae8164b846243743688ab4cb6b
florian m <florian.mathis@students.fhv.at>
2015-05-29 10:56:21 +0200

    init commit for filesystem

---
+++ src/system/filesystem/fat/time.h
@@ -0,0 +1,24 @@
+/**
+ * © 2011 Paul Annesley MIT license; free open source.
+ * https://github.com/pda/cfat16
+ */
+
+#pragma once
+
+#include <stdint.h>
+
+struct fat_date {
+    unsigned short int day;
+    unsigned short int month;
+    unsigned short int year;
+};
+
+struct fat_time {
+    unsigned short int second;
+    unsigned short int minute;
+    unsigned short int hour;
+};
+
+void fat_read_date(struct fat_date *, uint16_t bitfield);
+
+void fat_read_time(struct fat_time *, uint16_t bitfield);
