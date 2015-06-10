046ebf4bb6e69e415dde16f5af3586a6b9ba5479
Philip Heimböck <philipheimboeck@gmail.com>
2015-06-10 10:05:56 +0200

    i2c header added
    We will need TPS65950 for audio which is controlled by I2C

---
+++ src/system/hal/omap3530/i2c/i2c.h
@@ -0,0 +1,22 @@
+/*
+ * i2c.h
+ *
+ *  Created on: 10.07.2015
+ *      Author: philip
+ */
+
+#ifndef SRC_SYSTEM_HAL_OMAP3530_I2C_I2C_H_
+#define SRC_SYSTEM_HAL_OMAP3530_I2C_I2C_H_
+
+/*
+ * BeagleBoard System Reference 8.4.3 page 49
+ *
+ * OMAP3530 I2C Control
+ * The various components in the TPS65950 are controlled from the OMAP3530 via the
+ * I2C interface. I2C_0 is used to control the TPS65950 device.
+ *
+ * page: 50
+ * I2C interface seems to be I2C4
+ */
+
+#endif
