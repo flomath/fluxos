782a82c6a8445583af0cc4521480d311bca79e59
Philip Heimböck <philipheimboeck@gmail.com>
2015-07-03 22:54:40 +0200

    Creating and Destroying Semaphores

--- src/system/ipc/semaphore.c
+++ src/system/ipc/semaphore.c
@@ -7,6 +7,79 @@
 
 #include "semaphore.h"
 #include "../scheduler/scheduler.h"
+#include <string.h>
+
+sem_node_t* sem_list;
+
+sem_t* sem_create(const char* name) {
+	sem_t* sem = (sem_t*) malloc(sizeof(sem_t));
+	strcpy(sem->name, name);
+	sem->queue = NULL;
+	sem->value = 0;
+
+	sem_node_t* node = (sem_node_t*) malloc(sizeof(sem_node_t));
+
+	node->sem = sem;
+	node->next = NULL;
+
+	if (sem_list == NULL) {
+		// Empty list
+		sem_list = node;
+		return sem;
+	}
+
+	// Enqueue to the very last node element
+	sem_node_t* current = sem_list;
+	while (current->next != NULL) {
+		current = current->next;
+	}
+	current->next = node;
+
+	return sem;
+}
+
+void sem_destroy(const char* name) {
+	if (sem_list == NULL) {
+		// Empty list
+		return;
+	}
+
+	sem_node_t* prev = NULL;
+	sem_node_t* current = sem_list;
+	while (current->next != NULL && strcmp(name, current->sem->name) != 0) {
+		prev = current;
+		current = current->next;
+	}
+
+	if(strcmp(name, current->sem->name) == 0) {
+		if(prev == NULL) {
+			sem_list = sem_list->next;
+		} else {
+			prev = current->next;
+		}
+	}
+
+	free(current->sem);
+	free(current);
+}
+
+sem_t* sem_get(const char* name) {
+	if (sem_list == NULL) {
+		// Empty list
+		return NULL;
+	}
+
+	sem_node_t* current = sem_list;
+	while (current->next != NULL && strcmp(name, current->sem->name) != 0) {
+		current = current->next;
+	}
+
+	if(strcmp(name, current->sem->name) == 0) {
+		return current->sem;
+	}
+
+	return NULL;
+}
 
 int sem_wait(sem_t* sem) {
 	atom_begin();
--- src/system/ipc/semaphore.h
+++ src/system/ipc/semaphore.h
@@ -8,18 +8,39 @@
 #ifndef SRC_SYSTEM_IPC_SEMAPHORE_H_
 #define SRC_SYSTEM_IPC_SEMAPHORE_H_
 
-typedef struct sem_node node_t;
+typedef struct sem_process_node node_t;
+typedef struct sem_node sem_node_t;
 
-struct sem_node {
+struct sem_process_node {
 	int pid;
 	node_t* next;
 };
 
 typedef struct {
 	int value;
+	char* name;
 	node_t* queue;
 } sem_t;
 
+struct sem_node {
+	sem_t* sem;
+	sem_node_t* next;
+};
+
+/**
+ * Create a new semaphore and return it
+ */
+sem_t* sem_create(const char* name);
+
+/**
+ * Find the semaphore and destroy it
+ */
+void sem_destroy(const char* name);
+
+/**
+ * Find the semaphore and return it
+ */
+sem_t* sem_get(const char* name);
 
 /**
  * Wait until semaphore is released
